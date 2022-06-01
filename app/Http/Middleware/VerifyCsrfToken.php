c832430fbe96b4d8d3a762b1598f9834f08af454
g14wx <carlos.hernandez3@catolica.edu.sv>
2022-05-31 21:56:33 -0600

    fix: Add fix for CSRF, get rid of routes on middleware

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,12 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/register',
-        '/login',
-        '/forgot-password',
-        '/reset-password',
-        '/verify-email/{id}/{hash}',
-        '/email/verification-notification',
-        '/logout'
+
     ];
 }
