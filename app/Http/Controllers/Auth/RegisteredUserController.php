56b168e5681d71fb082887df2b0d16f78a53baec
g14wx <carlos.hernandez3@catolica.edu.sv>
2022-06-01 02:32:26 -0600

    fix: Adding last fixes for phone number and google login

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -38,7 +38,7 @@ class RegisteredUserController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'phone_number' => 'required'
+            'phone' => 'required'
         ]);
 
         $user = User::create([
